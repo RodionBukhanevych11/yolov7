1191ba8f9835050eb0c8550434e1a7ad681e1177
rodionbukhanevych11 <rodionbukhanevych@gmail.com>
2023-08-28 16:07:43 +0300

    submodule update

--- train.py
+++ train.py
@@ -507,7 +507,7 @@ if __name__ == '__main__':
     parser.add_argument('--data', type=str, default='data/data.yaml', help='data.yaml path')
     parser.add_argument('--hyp', type=str, default='data/hyp.scratch.p5.yaml', help='hyperparameters path')
     parser.add_argument('--epochs', type=int, default=300)
-    parser.add_argument('--batch-size', type=int, default=5, help='total batch size for all GPUs')
+    parser.add_argument('--batch-size', type=int, default=20, help='total batch size for all GPUs')
     parser.add_argument('--img-size', nargs='+', type=int, default=[736, 736], help='[train, test] image sizes')
     parser.add_argument('--rect', action='store_true', help='rectangular training')
     parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training')
